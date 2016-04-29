7f8f3f419f9a49252f12ef25e65fd888882c0d68
Luke Horvat <lukehorvat@gmail.com>
2016-04-29 12:55:09 +1000

    Remove build step

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
-export default function ({ types: t }) {
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+exports.default = function (_ref) {
+  var t = _ref.types;
+
   return {
     visitor: {
-      MemberExpression(path, state) {
+      MemberExpression: function MemberExpression(path, state) {
         if (path.matchesPattern("process.env.NODE_ENV")) {
           path.replaceWith(t.valueToNode(process.env.NODE_ENV || state.opts.default));
 
           if (path.parentPath.isBinaryExpression()) {
-            let evaluated = path.parentPath.evaluate();
+            var evaluated = path.parentPath.evaluate();
             if (evaluated.confident) {
               path.parentPath.replaceWith(t.valueToNode(evaluated.value));
             }
@@ -15,4 +23,4 @@ export default function ({ types: t }) {
       }
     }
   };
-}
+};
