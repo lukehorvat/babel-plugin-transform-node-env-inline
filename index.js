b6034b497fe704105edd4f4f3f37ff25edb42dc2
Luke Horvat <lukehorvat@gmail.com>
2016-10-28 14:16:33 +1100

    Simplify

--- index.js
+++ index.js
@@ -1,20 +1,17 @@
 "use strict";
 
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-
-exports.default = function (_ref) {
-  var t = _ref.types;
+module.exports = function(babel) {
+  var t = babel.types;
 
   return {
     visitor: {
-      MemberExpression: function MemberExpression(path, state) {
+      MemberExpression: function(path, state) {
         if (path.matchesPattern("process.env.NODE_ENV")) {
           path.replaceWith(t.valueToNode(process.env.NODE_ENV || state.opts.default));
 
           if (path.parentPath.isBinaryExpression()) {
             var evaluated = path.parentPath.evaluate();
+
             if (evaluated.confident) {
               path.parentPath.replaceWith(t.valueToNode(evaluated.value));
             }
